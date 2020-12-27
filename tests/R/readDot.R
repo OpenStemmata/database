dabb318be93cd9b2644a3c716a2c2a3dc494bded
Jean-Baptiste Camps <jbcamps@hotmail.com>
2020-12-27 17:45:06 +0100

    now as warnings

--- tests/R/readDot.R
+++ tests/R/readDot.R
@@ -1,12 +1,12 @@
 readDot <- function(filename) {
   if(missing(filename)){
-    stop("readDot: You have to provide something!")
+    warning(paste(filename, "You have to provide something!"))
   } else {
     print(paste("readDot: now reading", filename))
     inputData <- readLines(filename);
   }
   if(length(inputData) == 0){
-    stop("readDot: No content found!")
+    warning(paste(filename, "No content found!"))
   }
   
   # Clean starting and trailing whitespace
@@ -20,15 +20,15 @@ readDot <- function(filename) {
   
   # Let's go sanity checks
   if(!inputData[1] == "digraph {"){
-    stop("readDot: file should contain a directed graph (start with) digraph {")
+    warning(paste(filename, "should contain a directed graph (start with) digraph {"))
   }
   # content lines ()
   if(!all(grepl('[A-Za-z0-9]+\\s*\\->\\s*[[A-Za-z0-9]+\\;?|[A-Za-z0-9]+\\s*\\[(color=\\"grey\\"|label=\\"[^\\"]+\\"|color=\\"grey\\", label=\\"[^\\"]+\\")\\];?', inputData[2:(length(inputData)-1)]))){
     malformed = grep('[A-Za-z0-9]+\\s*\\->\\s*[[A-Za-z0-9]+\\;?|[A-Za-z0-9]+\\s*\\[(color=\\"grey\\"|label=\\"[^\\"]+\\"|color=\\"grey\\", label=\\"[^\\"]+\\")\\];?', inputData[2:(length(inputData)-1)], invert = TRUE)
-    stop(paste("readDot: lines not well formed, ", paste(malformed+1, collapse = " ")))
+    warning(paste(filename, "lines not well formed, ", paste(malformed+1, collapse = " ")))
   }
   if(!inputData[length(inputData)] == "}"){
-    stop("readDot: file should end with }")
+    warning(paste(filename, "file should end with }"))
   }
   # and now code to actually parse it
   print("all clear !")
