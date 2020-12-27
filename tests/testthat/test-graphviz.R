1a6dd3ba6db96ad808068bd9b575608fc245790a
Jean-Baptiste Camps <jbcamps@hotmail.com>
2020-12-27 16:23:11 +0100

    tests for graphviz integrity

--- tests/testthat/test-graphviz.R
+++ tests/testthat/test-graphviz.R
@@ -1,16 +1,10 @@
 test_that("all graphviz files are valid", {
   
   myGVs = Sys.glob("../../data/*/*/*.gv")
+  source("../R/readDot.R")
   
   for(i in 1:length(myGVs)){
-    #adj = sna::read.dot(myGVs[i])
-    #adj = sna::read.dot("stemma.gv")
-    DiagrammeR::grViz("stemma.gv")
-    DiagrammeR::DiagrammeR("stemma.gv", type = "grViz")
-    #sna::gplot()
-    
-    #plot(igraph::graph_from_adjacency_matrix(adj, mode = "directed"))
+    readDot(myGVs[i])
   }
-  
   
 })
