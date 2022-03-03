bc3a5a51e03e883f2dfb78d985d4fc2219fe6acb
Jean-Baptiste Camps <jbcamps@hotmail.com>
2022-03-03 17:35:54 +0100

    repeat fields when necessary

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,10 +4,8 @@
 library("testthat")
 options(testthat.progress.max_fails = 100)
 testthat::test_dir("tests/testthat/", stop_on_warning = TRUE)
-
 #library("igraph")
 #igraph::write_graph()
 # Read a dot file, with help from sna
 #sna::read.dot
-
 #sna::read.dot("../examples/Paris_1872_Alexis/stemma.gv")
